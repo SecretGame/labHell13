ac2d03149c906f5b160b2f4c95492f326d97c9a1
GGgun2013 <gggun2013@gmail.com>
2021-01-22 16:57:33 +0700

    lab13_2Clear

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -19,3 +19,47 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool C[][N]){
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+        {
+            cout<<C[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+void inputMatrix(double C[][N]){
+    for(int i=0;i<N;i++)
+    {
+        cout<<"Row "<<i+1<<": ";
+        for(int j=0;j<N;j++)
+        {
+        cin>>C[i][j];
+        }
+    }
+}
+
+void findLocalMax(const double A[][N], bool B[][N])
+{
+    for(int a=0;a<N;a++)
+    {
+        for(int b=0;b<N;b++)
+        {
+            B[a][b]=0;
+        }
+    }
+    
+    for(int a=1;a<N-1;a++)
+    {
+        for(int b=1;b<N-1;b++)
+        {
+            if(A[a][b]>=A[a-1][b] && A[a][b]>=A[a][b-1] && A[a][b]>=A[a+1][b] && A[a][b]>=A[a][b+1])
+            {
+                B[a][b]=1;
+            }
+        }
+    }
+}
